d8fe3d59d6343c961baa47b4e794ba39b620ced9
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-06-19 18:00:40 +0200

    fix limits test

--- test/src/limits.js
+++ test/src/limits.js
@@ -3,9 +3,8 @@ import test from 'ava';
 import { min , max , get64 } from '../../src' ;
 
 function macro (t, input, expected) {
-	const a = get64(...input);
 	const b = get64(...expected);
-	t.deepEqual(a, b);
+	t.deepEqual(input, b);
 }
 
 macro.title = (providedTitle, input, expected) => `${providedTitle} ${input} === ${expected}`.trim();
